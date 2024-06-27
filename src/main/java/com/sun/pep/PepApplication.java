a3d7c12a53b51c37cf3efa113a5e5f03282d4772
Mauro Peixoto <96122844+mauropeixotodev@users.noreply.github.com>
2024-06-27 19:05:58 -0300

    Fisrt commit

---
+++ src/main/java/com/sun/pep/PepApplication.java
@@ -0,0 +1,13 @@
+package com.sun.pep;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PepApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PepApplication.class, args);
+	}
+
+}
